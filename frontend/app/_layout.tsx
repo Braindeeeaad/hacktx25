30d5fa3d0b30e23f191d6ed17fb36150e905b26d
aryanjverma <aryanjverma2@gmail.com>
2025-10-18 13:54:09 -0500

    structures app into 4 main pages

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,61 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { useColorScheme } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colorScheme === 'dark' ? '#fff' : '#000',
+        tabBarStyle: {
+          backgroundColor: colorScheme === 'dark' ? '#000' : '#fff',
+        },
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="EmotionLogging"
+        options={{
+          title: 'Wellbeing',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="person" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="FinancialPage"
+        options={{
+          title: 'My Finances',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="settings" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Login"
+        options={{
+          title: 'Login',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="info" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof MaterialIcons>['name'];
+  color: string;
+}) {
+  return <MaterialIcons size={28} style={{ marginBottom: -3 }} {...props} />;
 }
